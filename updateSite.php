00a4e69ce8c615c1fbeceee79f100b9f671476aa
Jean Wasilewski <henyxia@live.fr>
2015-04-20 07:33:18 +0000

    Bad wget

--- updateSite.php
+++ updateSite.php
@@ -1,5 +1,5 @@
 <?php
-if(file_put_contents("git/master.zip", fopen("https://github.com/henyxia/revs0/archive/master.zip", 'r')) == false)
+if(file_put_contents("master.zip", fopen("https://github.com/henyxia/revs0/archive/master.zip", 'r')) == false)
     echo 1;
 else
     echo 0;
