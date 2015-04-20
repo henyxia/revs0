d07e68cfc49bfcd8a7ab5bf81e4f0951a30288d0
Jean Wasilewski <henyxia@live.fr>
2015-04-20 14:13:49 +0000

    Extracting ZIP

--- updateSite.php
+++ updateSite.php
@@ -1,5 +1,17 @@
 <?php
 if(file_put_contents("../git/master.zip", fopen("https://github.com/henyxia/revs0/archive/master.zip", 'r')) == false)
-    echo 1;
+    die("Unable to download the last update");
+$file = '../git/master.zip';
+$path = pathinfo(realpath($file), PATHINFO_DIRNAME);
+$zip = new ZipArchive;
+$res = $zip->open($file);
+if ($res === TRUE)
+{
+  $zip->extractTo($path);
+  $zip->close();
+  echo "$file was successfully extracted to $path";
+}
 else
-    echo 0;
+{
+  die("Error, couldn't open $file");
+}
