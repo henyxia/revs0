f3d9ea1d66d9e89a3d435f3d5c5c291089724a14
Jean Wasilewski <henyxia@live.fr>
2015-04-22 21:10:50 +0000

    New home

--- controller.php
+++ controller.php
@@ -1,6 +1,6 @@
 <?php
 if($_GET["url"] == "")
-    set_include_path(set_include_path()."/home/ovh/shared/__ovh/fr/welcome.html");
+    include("index.php");
 else
-    require_once("globals.php")
+    require_once("globals.php");
 ?>
