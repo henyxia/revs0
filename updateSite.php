728b0d715a34afd10e6a8c066cfacc628a72bddc
Jean Wasilewski <henyxia@live.fr>
2015-04-22 08:53:04 +0000

    Even more expressive

--- updateSite.php
+++ updateSite.php
@@ -1,9 +1,9 @@
 <?php
 echo "<pre>";
-echo "Getting last update";
+echo "Getting last update\n";
 if(file_put_contents("../git/master.zip", fopen("https://github.com/henyxia/revs0/archive/master.zip", 'r')) == false)
     die("Unable to download the last update");
-echo "Extracting last update";
+echo "Extracting last update\n";
 $file = '../git/master.zip';
 $path = pathinfo(realpath($file), PATHINFO_DIRNAME);
 $zip = new ZipArchive;
@@ -12,13 +12,13 @@ if ($res === TRUE)
 {
   $zip->extractTo($path);
   $zip->close();
-  echo "$file was successfully extracted to $path";
+  echo "$file was successfully extracted to $path\n";
 }
 else
 {
   die("Error, couldn't open $file");
 }
-echo "Syncing last update";
+echo "Syncing last update\n";
 $source = '../git/revs0-master';
 $destination = '../www/';
 
@@ -37,6 +37,7 @@ foreach($sourceFiles as $file) {
         }
 
     }
+    echo "SYNC: ".$file." -> ". $destination . $baseFile."\n";
     copy($file, $destination . $baseFile);
 }
 echo "</pre>";
