78c89fd0d3cd0a08fbebf2668e7c768317fda149
Jean Wasilewski <henyxia@live.fr>
2015-04-24 08:53:24 +0000

    Fixed redirection

--- controller.php
+++ controller.php
@@ -2,7 +2,7 @@
 if($_GET["url"] == "")
     include("index.php");
 else if($_GET["url"] == "index.php")
-    header("Location: www.revs0.com");
+    header("Location: http://www.revs0.com");
 else
     require_once("globals.php");
 ?>
