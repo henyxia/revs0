b72e53c8444ecb1f8687ed2ed70152687148d647
Jean Wasilewski <henyxia@live.fr>
2015-05-19 09:44:55 +0000

    Do not exchange JS and CSS

--- breath.js
+++ breath.js
@@ -6,6 +6,6 @@ function breath()
     actuSec++;
     if(actuSec>100)
         actuSec = 0;
-    document.getElementById("home").style.boxShadow = "box-shadow: 0px 0px 10px rgb("+(155+actuSec)+","+(155+actuSec)+","+(155+actuSec)+")";
+    document.getElementById("home").style.boxShadow = "0px 0px 10px rgb("+(155+actuSec)+","+(155+actuSec)+","+(155+actuSec)+")";
     setTimeout('breath();','100');
 }
