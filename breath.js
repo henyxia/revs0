c423d54546c6f63773696f3d77891a6df105711f
Jean Wasilewski <henyxia@live.fr>
2015-05-19 09:34:57 +0000

    Wrong keyboard, wrong !

--- breath.js
+++ breath.js
@@ -4,5 +4,5 @@ function breath()
 {
     var date = new Date;
     var sec = date.getSeconds();
-    document.getElementById("home").style.boxShadow = "box-shadow: 0px 0px 10px rgb("+(195+var)+","+(195+var)+","+(195+var)+")";
+    document.getElementById("home").style.boxShadow = "box-shadow: 0px 0px 10px rgb("+(195+sec)+","+(195+sec)+","+(195+sec)+")";
 }
