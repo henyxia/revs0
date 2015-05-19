8f2750e57d4364c1f8cf56d85cb4f4f3f833dcb7
Jean Wasilewski <henyxia@live.fr>
2015-05-19 09:39:24 +0000

    Really heavy breathing

--- breath.js
+++ breath.js
@@ -1,8 +1,10 @@
-setTimeout('breath();','500');
+setTimeout('breath();','100');
+var actuSec = 0;
 
 function breath()
 {
-    var date = new Date;
-    var sec = date.getSeconds();
-    document.getElementById("home").style.boxShadow = "box-shadow: 0px 0px 10px rgb("+(195+sec)+","+(195+sec)+","+(195+sec)+")";
+    actuSec++;
+    if(actuSec>100)
+        actuSec = 0;
+    document.getElementById("home").style.boxShadow = "box-shadow: 0px 0px 10px rgb("+(155+actuSec)+","+(155+actuSec)+","+(155+actuSec)+")";
 }
