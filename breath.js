cbe4972bccb6140a8e70db2e95936454ff538698
Jean Wasilewski <henyxia@live.fr>
2015-05-19 11:08:43 +0000

    Really really centered

--- breath.js
+++ breath.js
@@ -10,8 +10,8 @@ function breathOnce()
     g = d.getElementsByTagName('body')[0],
     x = w.innerWidth || e.clientWidth || g.clientWidth,
     y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-    document.getElementById("home").style.top = y+"px";
-    document.getElementById("home").style.left = x+"px";
+    document.getElementById("home").style.top = x/2+"px";
+    document.getElementById("home").style.left = y/2+"px";
     breath();
 }
 
