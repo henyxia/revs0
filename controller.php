dd317add9af141381f12c0a45cbb54624a99e8e4
Jean Wasilewski <henyxia@live.fr>
2015-06-30 15:57:37 +0000

    Header edit

--- controller.php
+++ controller.php
@@ -4,7 +4,10 @@ if($_GET["url"] == "")
 else if($_GET["url"] == "index.php")
     header("Location: http://www.revs0.com");
 else if($_GET["url"] == "css/theme")
+{
+    header("Content-type: text/css");
     include("theme.css");
+}
 else if($_GET["url"] == "js/breath")
     include("breath.js");
 else if($_GET["url"] == "debug/globals")
