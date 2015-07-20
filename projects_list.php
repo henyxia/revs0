a3bf112c93affe0d306e6e351b429145dfcbed4e
Jean Wasilewski <henyxia@live.fr>
2015-07-20 11:00:13 +0200

    Project list done

--- projects_list.php
+++ projects_list.php
@@ -1,7 +1,42 @@
 <?php
-$projects["number"]		= 1;
-$projects["name"][0]	= "ADC for FPGA";
-$projects["prog"][0]	= false;
-$projects["elec"][0]	= true;
-$projects["fpga"][0]	= true;
-$projects["othe"][0]	= false;
+//$projects["number"]		= 7;
+$projects[0]["name"]	= "ADC for FPGA";
+$projects[0]["prog"]	= false;
+$projects[0]["elec"]	= true;
+$projects[0]["fpga"]	= true;
+$projects[0]["othe"]	= false;
+$projects[1]["name"]	= "Lego NXT MindStorm Grabber";
+$projects[1]["prog"]	= false;
+$projects[1]["elec"]	= true;
+$projects[1]["fpga"]	= true;
+$projects[1]["othe"]	= false;
+$projects[2]["name"]	= "Automatic robotic arm";
+$projects[2]["prog"]	= false;
+$projects[2]["elec"]	= true;
+$projects[2]["fpga"]	= true;
+$projects[2]["othe"]	= false;
+$projects[3]["name"]	= "VGA for FPGA";
+$projects[3]["prog"]	= false;
+$projects[3]["elec"]	= true;
+$projects[3]["fpga"]	= true;
+$projects[3]["othe"]	= false;
+$projects[4]["name"]	= "The tweek project";
+$projects[4]["prog"]	= false;
+$projects[4]["elec"]	= true;
+$projects[4]["fpga"]	= true;
+$projects[4]["othe"]	= false;
+$projects[5]["name"]	= "Stereo HiFi amplifier";
+$projects[5]["prog"]	= false;
+$projects[5]["elec"]	= true;
+$projects[5]["fpga"]	= true;
+$projects[5]["othe"]	= false;
+$projects[6]["name"]	= "Arduino radio";
+$projects[6]["prog"]	= false;
+$projects[6]["elec"]	= true;
+$projects[6]["fpga"]	= true;
+$projects[6]["othe"]	= false;
+usort($projects, "custom_sort");
+function custom_sort($a,$b)
+{
+	return $a['name']>$b['name'];
+}
