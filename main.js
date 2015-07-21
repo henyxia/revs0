2ecfa8657c0e6b8fa8eab7e525db0fb368d3b801
henyxia <henyxia@live.fr>
2015-07-21 20:06:34 +0200

    Class management

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 /*
 */
+function activateElements(elem)
+{
+	list = $("#projectList")[0];
+	
+	for(i=1; i<(list.childNodes.length-1);i++)
+		if(list.childNodes[i].childNodes[0].hasAttribute(elem))
+			list.childNodes[i].childNodes[0].className = "disabled";
+		else
+			list.childNodes[i].childNodes[0].className = "";
+}
