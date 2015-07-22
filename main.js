535c4e59634416088fcae4f5cc8d4f053a7633a3
henyxia <henyxia@live.fr>
2015-07-22 20:20:28 +0200

    Better timings

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 */
 function activateElements(elem)
 {
-	$("html, body").animate({ scrollTop: $('#page').offset().top }, 1000);
+	$("html, body").animate({ scrollTop: $('#page').offset().top }, 500);
 	list = $("#projectList")[0];
 	for(i=1; i<(list.childNodes.length-1);i++)
 		if(!list.childNodes[i].childNodes[0].hasAttribute(elem))
