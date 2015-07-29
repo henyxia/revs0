27e4b216595215e405a90fad959db662736fdc8b
Jean Wasilewski <henyxia@live.fr>
2015-07-29 16:59:43 +0200

    Separating projects from root files

--- projects_tools.php
+++ projects_tools.php
@@ -5,6 +5,6 @@ function getProjectName($projects, $folder, $name)
 	for($i=0; $i<count($projects); $i++)
 		if($projects[$i]["fold"] == $folder &&
 			$projects[$i]["arti"] == $name)
-			return $folder."_".$name.".php";
+			return "projects/".$folder."_".$name.".php";
 	return "project_not_found.php";
 }
