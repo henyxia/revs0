087100765a2873bfa1a4c35a16fb267a08c66c76
henyxia <henyxia@live.fr>
2015-08-08 16:23:38 +0200

    Added some headline styles

--- project_renderer.php
+++ project_renderer.php
@@ -9,9 +9,9 @@ function renderProject($project)
 	<div id="projectContentFinal"><?php
 $content = $project->content;
 // Processing headlines
-$content = preg_replace("/^[\=]{1}([ \w]+)\={1}/m", "<h1>$1</h1>", $content);
-$content = preg_replace("/^[\=]{2}([ \w]+)\={2}/m", "<h2>$1</h2>", $content);
-$content = preg_replace("/^[\=]{3}([ \w]+)\={3}/m", "<h3>$1</h3>", $content);
+$content = preg_replace("/^[\=]{1}([ \w\?]+)\={1}/m", "<h1>$1</h1>", $content);
+$content = preg_replace("/^[\=]{2}([ \w\?]+)\={2}/m", "<h2>$1</h2>", $content);
+$content = preg_replace("/^[\=]{3}([ \w\?]+)\={3}/m", "<h3>$1</h3>", $content);
 // Processing URLs
 $content = preg_replace("/\[((http\:\/\/|https\:\/\/|ftp\:\/\/)([\w\.\/\-\?\=]*))\]/m",
 	"<a href=\"$1\">[Link]</a>", $content);
